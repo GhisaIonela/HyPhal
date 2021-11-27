04b7c27ffd58cd2e0ec97bdaee90ba172e510613
Andreea <gabrian211@gmail.com>
2021-11-27 16:18:25 +0200

    Modified Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import com.company.domain.User;
 import com.company.repository.Repository;
 import com.company.repository.db.FriendshipDbRepository;
 import com.company.repository.db.UserDbRepository;
-import com.company.repository.file.UserFile;
 import com.company.service.FriendshipService;
 import com.company.service.Network;
 import com.company.service.UserService;
@@ -17,21 +16,11 @@ import com.company.validators.UserValidator;
 public class Main {
 
     public static void main(String[] args) {
-//        Repository<Long, User> userRepoFile = new UserFile("data/users.in", new UserValidator() );
-//        Repository<Long, Friendship> friendshipRepoFile = new FriendshipFile("data/friendships.in", new FriendshipValidator());
-//        UserService userService = new UserService(userRepoFile, friendshipRepoFile);
-//        FriendshipService friendshipService = new FriendshipService(friendshipRepoFile, userRepoFile);
-//        Network network = Network.getInstance();
-//        network.setUserRepository(userRepoFile);
-//        network.setFriendshipRepository(friendshipRepoFile);
-//        Ui ui = new Ui(userService, friendshipService, network);
-//        ui.run();
 
         Repository<Long, User> userRepoDb = new UserDbRepository("jdbc:postgresql://localhost:5432/laborator",
                 "postgres", "postgres", new UserValidator());
         Repository<Long, Friendship> friendshipRepoDb = new FriendshipDbRepository("jdbc:postgresql://localhost:5432/laborator",
                 "postgres", "postgres", new FriendshipValidator());
-
 
         UserService userService2 = new UserService(userRepoDb, friendshipRepoDb);
         FriendshipService friendshipService2 = new FriendshipService(friendshipRepoDb, userRepoDb);
@@ -40,8 +29,6 @@ public class Main {
         network.setUserRepository(userRepoDb);
         Ui ui = new Ui(userService2, friendshipService2, network);
         ui.run();
-
-
 
     }
 }
