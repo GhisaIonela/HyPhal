56ad29511cdc3203cf2ceddefb9fddcef0fabe9b
Andreea <gabrian211@gmail.com>
2021-12-13 22:52:12 +0200

    transfer project into jar

---
+++ src/module-info.java
@@ -0,0 +1,15 @@
+module Lab4{
+    exports com.company.config;
+    exports com.company.controller;
+    exports com.company.credentials;
+    exports com.company.domain;
+    exports com.company.dto;
+    exports com.company.exceptions;
+    exports com.company.graph;
+    exports com.company.repository.db;
+    exports com.company.service;
+    exports com.company.utils;
+    exports com.company.validators;
+    requires java.sql;
+
+}
