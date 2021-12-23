201d604382b5fcdf7008d4b16d5338fbefe3d6f8
Andreea <gabrian211@gmail.com>
2021-12-23 22:59:39 +0200

    send and display message

---
+++ src/main/java/com/example/networkgui/MessageController.java
@@ -0,0 +1,224 @@
+package com.example.networkgui;
+
+import com.company.domain.Message;
+import com.company.domain.User;
+import com.company.service.ConversationManager;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Side;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class MessageController extends SuperController implements Initializable {
+    @FXML
+    private ScrollPane users;
+    @FXML
+    private VBox vbox_users;
+    @FXML
+    private Button button_send;
+    @FXML
+    private TextField tf_message;
+    @FXML
+    private VBox vbox_messages;
+    @FXML
+    private ScrollPane sp_main;
+    @FXML
+    private Button alina;
+    @FXML
+    private Button ana;
+
+    private ConversationManager chatroom;
+
+
+    private void createUsersButtons(){
+        Iterable<User> userList = controller.findAllUsers();
+        for(User user: userList){
+            Button userButton = new Button();
+            userButton.setText(user.getFirstName() + " " + user.getLastName());
+            vbox_users.getChildren().add(userButton);
+            userButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    chatroom = controller.createConversation(user.getEmail());
+                    loadChatHistory();
+                }
+            });
+        }
+
+    }
+
+
+    private void createNewMsgHBox(Pos alignment, String messageToSend, String style){
+        HBox hBox = new HBox();
+        hBox.setAlignment(alignment);
+        hBox.setPadding(new Insets(5,5,5,10));
+        Text text = new Text(messageToSend);
+        TextFlow textFlow = new TextFlow(text);
+
+        textFlow.setStyle(style);
+
+        textFlow.setPadding(new Insets(5,10,5,10));
+        text.setFill(Color.color(0.934, 0.945, 0.996));
+
+
+        MenuItem replay = new MenuItem("replay");
+        MenuItem delete = new MenuItem("delete");
+        ContextMenu contextMenu = new ContextMenu();
+
+        contextMenu.getItems().add(replay);
+        contextMenu.getItems().add(delete);
+        contextMenu.setStyle("-fx-background-color: grey;");
+
+        Button button = new Button();
+        button.setGraphic(textFlow);
+        button.setContextMenu(contextMenu);
+        button.setStyle("-fx-border-color: transparent;\n" +
+                "    -fx-border-width: 0;\n" +
+                "    -fx-background-radius: 0;\n" +
+                "    -fx-background-color: transparent;");
+//        button.hoverProperty().addListener((ChangeListener<Boolean>) (observable, oldValue, newValue) -> {
+//            //menuButton.hide();
+//            if (newValue) {
+//                button.getContextMenu().show(button, Side.BOTTOM, -15, -20);
+//            } else {
+//                button.getContextMenu().hide();
+//            }
+//        });
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                button.getContextMenu().show(button, Side.BOTTOM, -15, -20);
+            }
+        });
+        hBox.getChildren().add(button);
+        vbox_messages.getChildren().add(hBox);
+
+//        Menu menu = new Menu();
+//        menu.getItems().add(replay);
+//        Button button = new Button();
+//        MenuBar menuBar = new MenuBar();
+//        menuBar.getMenus().add(menu);
+//        menuBar.setStyle("-fx-border-color: transparent;\n" +
+//                "    -fx-border-width: 0;\n" +
+//                "    -fx-background-radius: 0;\n" +
+//                "    -fx-background-color: transparent;");
+//        button.setGraphic(menuBar);
+//        button.setStyle("-fx-border-color: transparent;\n" +
+//                "    -fx-border-width: 0;\n" +
+//                "    -fx-background-radius: 0;\n" +
+//                "    -fx-background-color: transparent;");
+
+        //hBox.getChildren().add(menuBar);
+
+//        MenuButton menuButton = new MenuButton();
+//        menuButton.setStyle("-fx-border-color: transparent;\n" +
+//                "    -fx-border-width: 0;\n" +
+//                "    -fx-background-radius: 0;\n" +
+//                "    -fx-background-color: transparent;");
+//
+//        //menuButton.setGraphic(textFlow);
+//        menuButton.getItems().add(replay);
+
+
+//        button.hoverProperty().addListener((ChangeListener<Boolean>) (observable, oldValue, newValue) -> {
+//            //menuButton.hide();
+//            if (newValue) {
+//                //menuButton.show();
+//                menu.show();
+//            } else {
+//               // menuButton.hide();
+//                menu.hide();
+//            }
+//        });
+
+
+//        textFlow.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent event) {
+//                menu.show();
+//            }
+//        });
+//
+//
+//        hBox.getChildren().add(textFlow);
+//        hBox.getChildren().add(button);
+//        //hBox.getChildren().add(menuButton);
+//
+//        vbox_messages.getChildren().add(hBox);
+    }
+
+
+
+    private void sendMsg(){
+        button_send.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String messageToSend = tf_message.getText();
+                if(!messageToSend.isEmpty()){
+                    String style = "-fx-color: rgb(239, 242, 255); "+
+                            "-fx-background-color: rgb(15,125,242);"+
+                            "-fx-background-radius: 20px";
+                    createNewMsgHBox(Pos.CENTER_RIGHT, messageToSend, style);
+
+                    //send msg to db
+                    chatroom.sendMessage(messageToSend);
+
+                    tf_message.clear();
+                }
+            }
+        });
+    }
+
+    private void loadChatHistory(){
+        vbox_messages.getChildren().clear();
+        List<Message> messages = chatroom.getMessageList();
+        for (Message message: messages) {
+            if(Objects.equals(message.getFrom().getId(), chatroom.getReceiver().getId())){
+                String style = "-fx-color: rgb(239, 242, 255); "+
+                        "-fx-background-color: rgb(200,25,142);"+
+                        "-fx-background-radius: 20px";
+                createNewMsgHBox(Pos.CENTER_LEFT, message.getMessage(), style);
+
+            }else{
+                String style2 = "-fx-color: rgb(239, 242, 255); "+
+                        "-fx-background-color: rgb(15,125,242);"+
+                        "-fx-background-radius: 20px";
+                createNewMsgHBox(Pos.CENTER_RIGHT, message.getMessage(), style2);
+            }
+
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        vbox_messages.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                sp_main.setVvalue((Double) newValue);
+            }
+        });
+        createUsersButtons();
+        //changeRoom();
+        sendMsg();
+
+
+    }
+}
