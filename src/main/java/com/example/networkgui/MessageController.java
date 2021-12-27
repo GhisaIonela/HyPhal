079a7db4fae22e91e6dc2aa6eb13c5d94b6ef789
Andreea <gabrian211@gmail.com>
2021-12-27 16:43:06 +0200

    Added datetime for displaying messages

--- src/main/java/com/example/networkgui/MessageController.java
+++ src/main/java/com/example/networkgui/MessageController.java
@@ -2,6 +2,7 @@ package com.example.networkgui;
 
 import com.company.domain.Message;
 import com.company.domain.User;
+import com.company.dto.UserDTO;
 import com.company.listen.Listener;
 import com.company.service.ConversationManager;
 import javafx.animation.KeyFrame;
@@ -9,8 +10,8 @@ import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableMap;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -19,8 +20,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.SubScene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -35,11 +36,16 @@ import org.controlsfx.control.PopOver;
 import org.postgresql.PGNotification;
 
 import java.net.URL;
-import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static javafx.scene.paint.Color.rgb;
 
@@ -56,9 +62,21 @@ public class MessageController extends SuperController implements Initializable 
     private VBox vbox_messages;
     @FXML
     private ScrollPane sp_main;
+    @FXML
+    private TableView<UserDTO> tableUsers;
+    @FXML
+    private TableColumn<UserDTO, String> columnUser;
+    @FXML
+    private TextField searchUserTextField;
+    @FXML
+    private Label chatWithLabel;
 
     private ConversationManager chatroom;
+    private ObservableList<UserDTO> modelUser = FXCollections.observableArrayList();
 
+    public MessageController() {
+        modelUser.setAll(getUserDTOList());
+    }
 
     private void listenToNewMessageForCurrentRoom() {
         try {
@@ -101,36 +119,21 @@ public class MessageController extends SuperController implements Initializable 
         }
     }
 
-    private void createUsersButtons(){
-        Iterable<User> userList = controller.findAllUsers();
-        for(User user: userList){
-            Button userButton = new Button();
-            userButton.setText(user.getFirstName() + " " + user.getLastName());
-            vbox_users.getChildren().add(userButton);
-            userButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    chatroom = controller.createConversation(user.getEmail());
-                    loadChatHistory();
-                }
-            });
-        }
-
-    }
-
 
     private HBox createNewMsgHBox(Pos alignment, Message message, String style){
         HBox hBox = new HBox();
         hBox.setAlignment(alignment);
         hBox.setPadding(new Insets(5,5,5,10));
         Text text = new Text(message.getMessage());
-        text.setFont(new Font(14));
+        text.setFont(new Font(16));
         TextFlow textFlow = new TextFlow(text);
 
         textFlow.setStyle(style);
 
         textFlow.setPadding(new Insets(5,10,5,10));
         text.setFill(Color.color(0.934, 0.945, 0.996));
+
+        TextFlow dateTime = displayMessageDateTime(message, style);
 
         if(message.getReplay()!=null){
             Label label = new Label();
@@ -152,12 +155,18 @@ public class MessageController extends SuperController implements Initializable 
             bounds.getChildren().add(label);
             bounds.getChildren().add(replay);
             bounds.getChildren().add(textFlow);
+            bounds.getChildren().add(dateTime);
+
             replayWrap.getChildren().add(bounds);
 
             hBox.getChildren().add(replayWrap);
         }
         else{
-            hBox.getChildren().add(textFlow);
+            VBox bounds = new VBox();
+            bounds.setStyle(style);
+            bounds.getChildren().add(textFlow);
+            bounds.getChildren().add(dateTime);
+            hBox.getChildren().add(bounds);
         }
 
        messageOptionPopup(textFlow);
@@ -167,8 +176,20 @@ public class MessageController extends SuperController implements Initializable 
 
     private void messageOptionPopup(TextFlow textFlow){
         VBox options = new VBox();
-        Button replay = new Button("replay");
-        Button delete = new Button("delete");
+        ImageView replayImg = new ImageView(new Image("images/replay.png"));
+        replayImg.setFitHeight(15);
+        replayImg.setFitWidth(15);
+        Button replay = new Button();
+        replay.setStyle("-fx-background-color: transparent");
+        replay.setGraphic(replayImg);
+
+        ImageView deleteImg = new ImageView(new Image("images/delete.png"));
+        deleteImg.setFitHeight(15);
+        deleteImg.setFitWidth(15);
+        Button delete = new Button();
+        delete.setStyle("-fx-background-color: transparent");
+        delete.setGraphic(deleteImg);
+
         options.getChildren().add(replay);
         options.getChildren().add(delete);
 
@@ -209,8 +230,7 @@ public class MessageController extends SuperController implements Initializable 
 
     private void replayToMsg(TextFlow textFlow){
         String indexString = null;
-        //HBox parentText = (HBox) textFlow.getParent();
-        if(textFlow.getParent() instanceof HBox){
+        if(textFlow.getParent().getParent() instanceof HBox){
             indexString = textFlow.getParent().getId();
         }else{
             indexString = textFlow.getParent().getParent().getParent().getId();
@@ -224,10 +244,17 @@ public class MessageController extends SuperController implements Initializable 
         Popup popupReplay = new Popup();
         TextField textField = new TextField();
         HBox bound = new HBox();
-        Button sendReplay = new Button("replay");
+
+        ImageView replayImg = new ImageView(new Image("images/send.png"));
+        replayImg.setFitHeight(10);
+        replayImg.setFitWidth(10);
+        Button sendReplay = new Button();
+        sendReplay.setGraphic(replayImg);
+
         Button cancel = new Button();
         ImageView imageView = new ImageView();
         imageView.setImage(new Image("images/cancel.png"));
+
         imageView.setFitHeight(10);
         imageView.setFitWidth(10);
         cancel.setGraphic(imageView);
@@ -266,20 +293,34 @@ public class MessageController extends SuperController implements Initializable 
 
     }
 
+    private TextFlow displayMessageDateTime(Message message, String style){
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("E HH:mm");
+        String dateTime = message.getDateTime().format(dtf).toString();
+
+        Text text = new Text(dateTime);
+        text.setFill(Color.color(0.934, 0.945, 0.996));
+        text.setFont(new Font(11));
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setPadding(new Insets(5,10,5,10));
+        textFlow.setStyle(style);
+
+        return textFlow;
+    }
+
     private void sendMsg(){
         button_send.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 String messageToSend = tf_message.getText();
                 Message message = new Message(null,null, messageToSend);
+                message.setDateTime(LocalDateTime.now());
                 if(!messageToSend.isEmpty()){
                     String style = "-fx-color: rgb(239, 242, 255); "+
                             "-fx-background-color: rgb(27,77,62);"+
                             "-fx-background-radius: 20px";
                     HBox hBox = createNewMsgHBox(Pos.CENTER_RIGHT, message, style);
                     vbox_messages.getChildren().add(hBox);
-
-                    //send msg to db
                     chatroom.sendMessage(messageToSend);
 
                     tf_message.clear();
@@ -311,18 +352,62 @@ public class MessageController extends SuperController implements Initializable 
         }
     }
 
+    private void customSendButton(){
+        ImageView imageView = new ImageView(new Image("images/send.png"));
+        imageView.setFitWidth(40);
+        imageView.setFitHeight(40);
+        button_send.setGraphic(imageView);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        customSendButton();
         vbox_messages.heightProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
                 sp_main.setVvalue((Double) newValue);
             }
         });
-        createUsersButtons();
+        columnUser.setPrefWidth(tableUsers.getPrefWidth() - 2);
+        columnUser.setCellValueFactory(new PropertyValueFactory<UserDTO, String>("fullInfo"));
+        tableUsers.setItems(modelUser);
+        searchUserTextField.textProperty().addListener(o->handleFilter());
         sendMsg();
         listenToNewMessageForCurrentRoom();
+        initChatRoom();
 
 
     }
+
+    private void handleFilter() {
+        Predicate<UserDTO> predicate = u -> u.getFullName().startsWith(searchUserTextField.getText());
+        modelUser.setAll(getUserDTOList()
+                .stream().filter(predicate)
+                .collect(Collectors.toList()));
+    }
+
+    private List<UserDTO> getUserDTOList() {
+        List<User> userList = StreamSupport.stream(controller.findAllUsers().spliterator(), false)
+                .collect(Collectors.toList());
+        return userList
+                .stream()
+                .map(u-> new UserDTO(u.getEmail(), u.getFirstName(), u.getLastName()))
+                .collect(Collectors.toList());
+    }
+
+    private void initChatRoom(){
+        tableUsers.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    if (newValue == null) {
+                        return;
+                    }
+                    chatWithLabel.setText(newValue.getFullInfo().split("\n")[0]);
+                    chatroom = controller.createConversation(newValue.getFullInfo().split("\n")[1]);
+                    loadChatHistory();
+
+                }
+        );
+
+    }
+
 }
