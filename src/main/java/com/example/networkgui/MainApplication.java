caa9c507a74dbc600422f89080deecbc2a8121a8
Andreea <gabrian211@gmail.com>
2021-12-29 18:14:50 +0200

    Made just one connection for database

--- src/main/java/com/example/networkgui/MainApplication.java
+++ src/main/java/com/example/networkgui/MainApplication.java
@@ -10,9 +10,6 @@ import com.company.service.*;
 import com.company.validators.FriendshipValidator;
 import com.company.validators.UserValidator;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -29,37 +26,37 @@ public class MainApplication extends Application {
         SuperController.setStage(stage);
         SceneController.switchToAnotherScene("login-view.fxml");
 
-
     }
 
     public static void main(String[] args) {
-        //DatabaseConnectionCredentials dbConnectCred = DatabaseConnectionCredentials.getInstance();
-        //UserDbRepository userRepoDb = new UserDbRepository(dbConnectCred.getUrl(), dbConnectCred.getUsername(), dbConnectCred.getPassword(), new UserValidator());
-        UserDbRepository userRepoDb = new UserDbRepository("jdbc:postgresql://localhost:5432/laborator", "postgres", "postgres", new UserValidator());
-        //FriendshipDbRepository friendshipRepoDb = new FriendshipDbRepository(dbConnectCred.getUrl(), dbConnectCred.getUsername(), dbConnectCred.getPassword(), new FriendshipValidator());
-        FriendshipDbRepository friendshipRepoDb = new FriendshipDbRepository("jdbc:postgresql://localhost:5432/laborator", "postgres", "postgres", new FriendshipValidator());
-        UserService userService2 = new UserService(userRepoDb, friendshipRepoDb);
-        FriendshipService friendshipService2 = new FriendshipService(friendshipRepoDb, userRepoDb);
-        Network network = Network.getInstance();
-        network.setFriendshipRepository(friendshipRepoDb);
-        network.setUserRepository(userRepoDb);
-        LoginManager loginManager = new LoginManager(userRepoDb);
-        //MessageDbRepository messageDbRepository = new MessageDbRepository(dbConnectCred.getUrl(), dbConnectCred.getUsername(), dbConnectCred.getPassword());
-        MessageDbRepository messageDbRepository = new MessageDbRepository("jdbc:postgresql://localhost:5432/laborator", "postgres", "postgres");
-        MessageService messageService = new MessageService(messageDbRepository, userRepoDb);
-        FriendRequestsDbRepository friendRequestsDbRepository = new FriendRequestsDbRepository("jdbc:postgresql://localhost:5432/laborator", "postgres", "postgres");
-        FriendRequestService friendRequestService = new FriendRequestService(userRepoDb, friendshipRepoDb, friendRequestsDbRepository);
-        Controller controller = new Controller(userService2, friendshipService2, network, loginManager, messageService, friendRequestService);
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/laborator", "postgres", "postgres");
+            DatabaseConnectionCredentials dbConnectCred = DatabaseConnectionCredentials.getInstance();
+            Connection connection = DriverManager.getConnection(dbConnectCred.getUrl(), dbConnectCred.getUsername(), dbConnectCred.getPassword());
+
+            UserDbRepository userRepoDb = new UserDbRepository(connection, new UserValidator());
+            FriendshipDbRepository friendshipRepoDb = new FriendshipDbRepository(connection, new FriendshipValidator());
+            UserService userService2 = new UserService(userRepoDb, friendshipRepoDb);
+            FriendshipService friendshipService2 = new FriendshipService(friendshipRepoDb, userRepoDb);
+            Network network = Network.getInstance();
+            network.setFriendshipRepository(friendshipRepoDb);
+            network.setUserRepository(userRepoDb);
+            LoginManager loginManager = new LoginManager(userRepoDb);
+            MessageDbRepository messageDbRepository = new MessageDbRepository(connection);
+            MessageService messageService = new MessageService(messageDbRepository, userRepoDb);
+            FriendRequestsDbRepository friendRequestsDbRepository = new FriendRequestsDbRepository(connection);
+            FriendRequestService friendRequestService = new FriendRequestService(userRepoDb, friendshipRepoDb, friendRequestsDbRepository);
+            Controller controller = new Controller(userService2, friendshipService2, network, loginManager, messageService, friendRequestService);
+
+
             SuperController.setConnection(connection);
+            SuperController.setController(controller);
+            SuperController.setLoginManager(loginManager);
+            launch();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        SuperController.setController(controller);
-        SuperController.setLoginManager(loginManager);
 
-        launch();
     }
 }
